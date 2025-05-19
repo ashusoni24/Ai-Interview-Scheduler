b08e96a77267026e3429f1b5edf654e53d79cca2
ashusoni24 <24ashusoni@gmail.com>
2025-05-19 23:47:55 +0530

    added the google authentication using supabse

---
+++ app/Provider.jsx
@@ -0,0 +1,52 @@
+    'use client';
+    import { UserDetailContext } from '@/context/userdetailcontext';
+    import { supabase } from '@/services/supabseclient';
+    import React, { useContext, useEffect, useState } from 'react'
+
+
+    function Provider({children}){
+
+        const [user,setUser] = useState(null);
+        useEffect(()=>{
+            CreateNewUser();
+        },[]) 
+
+        const CreateNewUser = () =>{
+            supabase.auth.getUser().then(async({data:{user}})=>{
+                //check is user exist or not
+                let { data: Users, error } = await supabase
+    .from('Users')
+    .select('*')
+    .eq('email',user?.email);
+        console.log(Users)
+        if(Users?.length == 0){
+        const { data, error }=  await supabase.from("Users").
+            insert([
+                {
+                    name:user?.user_metadata?.name,
+                    email:user?.email,
+                    picture:user?.user_metadata?.picture
+                }
+            ])
+            console.log(data);
+            setUser(data)
+            return;
+        }
+        setUser(Users[0]);
+            })
+        }
+
+        return (
+            <UserDetailContext.Provider value={{ user, setUser }}>
+                        <div>{children}</div>
+            </UserDetailContext.Provider>
+
+        )
+    }
+
+    export default Provider
+
+    export const useUser = ()=>{
+        const context = useContext(UserDetailContext);
+        return context;
+    }
---
+++ app/auth/page.jsx
@@ -0,0 +1,43 @@
+'use client';
+import { Button } from '@/components/ui/button'
+import { supabase } from '@/services/supabseclient'
+import Image from 'next/image'
+import React from 'react'
+
+function Login() {
+  /**
+   * used to sign in with google
+   */
+  const signInWithOAuth = async() => {
+    const {error} = await supabase.auth.signInWithOAuth({
+      provider:'google'
+    });
+    if(error){
+      console.error('Error:' ,error.message)
+    }
+  }
+  return (
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <div className='flex flex-col items-center border rounded-2xl p-8'>
+        <Image src={'/logo.png'} alt='logo' 
+        width={200} 
+        height={150}
+        className='w-[170px]'
+        ></Image>
+      
+      <div className='flex flex-col '>
+        <Image src={'/login.png'} alt='login'
+         width={600}
+          height={400}
+          className='w-[400px] h-[250px]'></Image>
+          <h2 className='text-2xl font-bold text-center mt-2'>Welcome to AICruiter</h2>
+          <p className='text-gray-500 text-center'>Sign In With Google Authentication</p>
+          <Button className='mt-4'
+          onClick={signInWithOAuth}>Login with Google</Button>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
