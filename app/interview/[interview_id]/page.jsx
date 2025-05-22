9ca4dd1a74e959d5c36648edef5ac9b69cc11b0d
ashusoni24 <24ashusoni@gmail.com>
2025-05-22 23:53:08 +0530

    creted the display page for users/candidates

---
+++ app/interview/[interview_id]/page.jsx
@@ -0,0 +1,92 @@
+'use client';
+import React, { useEffect, useState } from 'react'
+import InterviewHeader from '../_components/InterviewHeader'
+import Image from 'next/image'
+import { Clock, Info, Video } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { useParams } from 'next/navigation'
+import { supabase } from '@/services/supabseClient';
+import { toast } from 'sonner';
+
+
+function Interview() {
+
+   const {interview_id}= useParams();
+   console.log(interview_id)
+   
+   const[interviewData,setInterviewData]=useState();
+   const[username,setUserName]=useState();
+   const[loading,setLoading]= useState(false);
+
+    useEffect(()=>{
+    interview_id && GetInterviewDetails();
+    },[interview_id])
+
+   const GetInterviewDetails =async()=>{
+    setLoading(true);
+    try{
+    let { data:Interviews, error } = await supabase
+  .from('Interviews')
+  .select("jobPosition,jobDescription,duration,type")
+  .eq('interview_id',interview_id)
+   setInterviewData(Interviews[0]);
+   setLoading(false);
+   if(Interviews?.length==0){
+    toast('Incorrect Interview Link')
+    return ;
+   }
+   
+    }
+    catch(e){
+        setLoading(false);
+        touch('Incorrect Interview Link')
+    }
+
+       }
+
+  return (
+
+    <div className='mt-16 flex justify-center px-4'>
+         <div className='w-full max-w-2xl flex flex-col items-center justify-center border rounded-2xl bg-white p-9 mb-20'>
+            <Image src={'/logo.png'} alt='logo' width ={200} height={100}
+        className='w-[140px]'/>
+        <h2 className='mt-2'>
+           AI-Powered Interview Platform
+        </h2>
+             <Image src={'/interview2.png'} alt='interview'
+             width={500}
+             height={500}
+             className='w-[280px] my-4'/>
+
+             <h2 className='font-bold text-xl '>{interviewData?.jobPosition}</h2>
+             <h2 className='flex gap-2 items-center mt-2 text-gray-500'><Clock className='h-4 w-4'/>{interviewData?.duration}</h2>
+
+             <div className='w-full '>
+                <h2>Enter Your Full Name</h2>
+                <Input placeholder='e.g. Ashutosh Soni' className='mt-2' onChange={(event)=>setUserName(event.target.value)}/>
+             </div>
+
+              <div className='p-3 bg-blue-50 flex gap-4 rounded-lg mt-4'>
+                <Info className='text-primary'/>
+                <div>
+                    <h2 className='font-bold'>Before you begin </h2>
+                   <ul className=''>
+                        <li className='text-sm text-primary'>- Test you microphone before</li>
+                        <li className='text-sm text-primary'>- Ensure you have a stable connection</li>
+                        <li className='text-sm text-primary'>- Find a quiet place for interview</li>
+                        
+                </ul>
+             </div>
+
+              </div>
+
+              <Button className={'mt-5 w-full font-bold'}
+              disabled ={loading||!username}><Video/>Join Interview</Button>
+
+         </div>
+    </div>
+  )
+}
+
+export default Interview
