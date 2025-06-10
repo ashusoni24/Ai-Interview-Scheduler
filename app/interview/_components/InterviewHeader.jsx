142f2892564829e65d02e3aa56366815248aefaa
ashusoni24 <24ashusoni@gmail.com>
2025-06-10 21:28:22 +0530

    logo update

--- app/interview/_components/InterviewHeader.jsx
+++ app/interview/_components/InterviewHeader.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 
 function InterviewHeader() {
   return (
-    <header className="w-full flex justify-center items-center py-4 bg-blur backdrop-blur-md shadow-md border-b border-blue-100 sticky top-0 z-30">
+    <header className="w-full flex justify-center items-center py-4 bg-blur  bg-blue-50 backdrop-blur-md shadow-md border-b border-blue-100 sticky top-0 z-30">
       <Image
-        src="/logo.png"
+        src="/logo12.png"
         alt="logo"
         width={120}
         height={40}
-        className="w-[120px] h-auto"
+        className="w-[170px] h-auto"
         priority
       />
     </header>
